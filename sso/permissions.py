bfe945b57d08ec92b2ca804af59c1a67a67f2ccf
codiebeulaine <codiebeulaine@gmail.com>
2015-01-26 12:19:03 +0200

    added permissions.py

---
+++ sso/permissions.py
@@ -0,0 +1,17 @@
+from unicoresso.models import authorizedSite
+from django.contrib.auth.models import Group, User
+
+def custom_attributes(user, service):
+	# for group in request.user.groups.all():
+	# 	if (service in group.objects.all()):
+	# 		return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'yes'}
+	groups = user.groups.values_list('name',flat=True)
+	
+	website = authorizedSite.objects.get(site=service) 
+	site_groups =  website.group.values_list('name',flat=True)
+
+	for group in groups:
+		for site_group in site_groups:
+			if site_group == group:
+				return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'yes', 'service_name': service, 'group': group,}
+	return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'no', 'service_name': service, 'group': groups,}
