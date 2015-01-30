93452bb068ed00b951c2f5826906711b2b190858
codiebeulaine <codiebeulaine@gmail.com>
2015-01-30 09:48:02 +0200

    added tests

--- sso/permissions.py
+++ sso/permissions.py
@@ -2,16 +2,14 @@ from unicoresso.models import authorizedSite
 from django.contrib.auth.models import Group, User
 
 def custom_attributes(user, service):
-	# for group in request.user.groups.all():
-	# 	if (service in group.objects.all()):
-	# 		return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'yes'}
-	groups = user.groups.values_list('name',flat=True)
 	
+	groups = user.groups.values_list('name',flat=True)
 	website = authorizedSite.objects.get(site=service) 
 	site_groups =  website.group.values_list('name',flat=True)
 
-	for group in groups:
-		for site_group in site_groups:
-			if site_group == group:
-				return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'yes', 'service_name': service, 'group': group,}
-	return {'givenName': user.first_name, 'email': user.email, 'has_perm': 'no', 'service_name': service, 'group': groups,}
+	if groups and website:
+		for group in groups:
+			for site_group in site_groups:
+				if site_group == group:
+					return {'givenName': user.first_name, 'email': user.email, 'has_perm': True, 'service_name': service, 'group': group,}
+	return {'givenName': user.first_name, 'email': user.email, 'has_perm': False, 'service_name': service, 'group': groups,}
--- unicoresso/tests.py
+++ unicoresso/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from sso import permissions
+from django.contrib.auth.models import Group, User
+from unicoresso.models import authorizedSite
 
-# Create your tests here.
+class customAttributesTest(TestCase):
+	def test_group_access(self):
+		user = User.objects.create(username='foo')
+		site = authorizedSite.objects.create(site='foobar.com')
+		self.assertEqual(permissions.custom_attributes(user, site)['has_perm'], False)
+
+
+	def test_correct_user(self):
+		user = User.objects.create(username='foo')
+		site = authorizedSite.objects.create(site='foobar.com')
+		self.assertEqual(permissions.custom_attributes(user, site)['givenName'], 'foo')
+
+
+
+
+
