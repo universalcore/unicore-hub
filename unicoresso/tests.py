64c62b6860043bf0c10ae3822087d4ec2c3c5341
codiebeulaine <codiebeulaine@gmail.com>
2015-01-30 10:42:58 +0200

    correct tests

--- unicoresso/tests.py
+++ unicoresso/tests.py
@@ -5,17 +5,18 @@ from unicoresso.models import authorizedSite
 
 class customAttributesTest(TestCase):
 	def test_group_access(self):
-		user = User.objects.create(username='foo')
+		user = User.objects.create(first_name='foo')
 		site = authorizedSite.objects.create(site='foobar.com')
 		self.assertEqual(permissions.custom_attributes(user, site)['has_perm'], False)
 
-
 	def test_correct_user(self):
-		user = User.objects.create(username='foo')
+		user = User.objects.create(first_name='foo')
 		site = authorizedSite.objects.create(site='foobar.com')
 		self.assertEqual(permissions.custom_attributes(user, site)['givenName'], 'foo')
 
-
-
+	def test_correct_group(self):
+		user = User.objects.create(first_name=' ')
+		site = authorizedSite.objects.create(site='foobar.com')
+		self.assertEqual(permissions.custom_attributes(user, site)['group'].count(), 0)
 
 
