a747578c0941255efcb90eaf6d640d9eaaaf4bfc
Milton Madanda <madandat@gmail.com>
2015-02-02 10:25:08 +0200

    add wildcards to site urls

--- sso/permissions.py
+++ sso/permissions.py
@@ -1,26 +1,36 @@
 from unicoresso.models import AuthorizedSite
+from urlparse import urlparse
+import re
 
 
 def custom_attributes(user, service):
+    # e.g
+    # User: Joe Soap
+    # serice: http://cms.tz.fflangola.qa-hub.unicore.io/login/
 
-    groups = user.groups.values_list('name', flat=True)
+    service_domain = urlparse(service).netloc
 
-    if AuthorizedSite.objects.filter(site=service).exists():
-        website = AuthorizedSite.objects.get(site=service)
-        site_groups = website.group.values_list('name', flat=True)
+    sites = [
+        s for s in AuthorizedSite.objects.all()
+        if re.match(
+            re.compile(s.site.replace('.', '\\.').replace('*', '.*')),
+            service_domain)]
 
-        for group in groups:
-            for site_group in site_groups:
-                if site_group == group:
-                    return {
-                        'givenName': user.first_name,
-                        'email': user.email,
-                        'has_perm': True,
-                        'service_name': service,
-                        'group': group, }
+    if sites:
+        user_groups = set(user.groups.values_list('name', flat=True))
+        for site in sites:
+            site_groups = set(site.group.values_list('name', flat=True))
+            intersect = user_groups & site_groups
+            if intersect:
+                return {
+                    'givenName': user.first_name,
+                    'email': user.email,
+                    'has_perm': True,
+                    'service_name': service,
+                    'groups': list(intersect)}
     return {
         'givenName': user.first_name,
         'email': user.email,
         'has_perm': False,
         'service_name': service,
-        'group': groups, }
+        'groups': []}
--- unicoresso/tests/test_permissions.py
+++ unicoresso/tests/test_permissions.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from sso import permissions
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from unicoresso.models import AuthorizedSite
 
 
@@ -8,18 +8,46 @@ class customAttributesTest(TestCase):
 
     def test_group_access(self):
         user = User.objects.create(first_name='foo')
-        site = AuthorizedSite.objects.create(site='foobar.com')
+        site = AuthorizedSite.objects.create(site='http://foobar.com')
         self.assertEqual(
-            permissions.custom_attributes(user, site)['has_perm'], False)
+            permissions.custom_attributes(user, site.site)['has_perm'], False)
 
     def test_correct_user(self):
         user = User.objects.create(first_name='foo')
-        site = AuthorizedSite.objects.create(site='foobar.com')
+        site = AuthorizedSite.objects.create(site='http://foobar.com')
         self.assertEqual(
-            permissions.custom_attributes(user, site)['givenName'], 'foo')
+            permissions.custom_attributes(user, site.site)['givenName'], 'foo')
 
     def test_correct_group(self):
         user = User.objects.create(first_name=' ')
-        site = AuthorizedSite.objects.create(site='foobar.com')
+        site = AuthorizedSite.objects.create(site='http://foobar.com')
         self.assertEqual(
-            permissions.custom_attributes(user, site)['group'].count(), 0)
+            len(permissions.custom_attributes(user, site.site)['groups']), 0)
+
+    def test_wildcard_url(self):
+        user = User.objects.create(first_name='foo')
+        group = Group.objects.create(name='Unicef')
+        user.groups.add(group)
+        user.save()
+
+        site = AuthorizedSite.objects.create(
+            site='*.fflangola.qa-hub.unicore.io')
+        site.group.add(group)
+        site.save()
+
+        attr = permissions.custom_attributes(
+            user, 'htto://cms.tz.fflangola.qa-hub.unicore.io/login/')
+        self.assertEqual(attr['has_perm'], True)
+        self.assertEqual(attr['groups'], ['Unicef'])
+
+        attr = permissions.custom_attributes(
+            user, 'htto://cms.za.fflangola.qa-hub.unicore.io/login/')
+        self.assertEqual(attr['has_perm'], True)
+
+        attr = permissions.custom_attributes(
+            user, 'htto://cms.za.ffl.qa-hub.unicore.io/login/')
+        self.assertEqual(attr['has_perm'], False)
+
+        attr = permissions.custom_attributes(
+            user, 'htto://cms.za.gem.qa-hub.unicore.io/login/')
+        self.assertEqual(attr['has_perm'], False)
